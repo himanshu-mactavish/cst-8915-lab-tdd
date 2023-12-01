2d8409f7b40bb797608b6dd5e558bb1a24c8e4bb
Himanshu MacTavish <Himanshu.Mactavish@gmail.com>
2023-11-30 22:18:21 -0500

    Step-2 done

--- src/__init__.py
+++ src/__init__.py
@@ -2,14 +2,15 @@
 
 from flask import Flask,jsonify
 from flask_restx import Resource, Api
-
+import os
 #instantiate the app
 app=Flask(__name__)
 
 api=Api(app)
 
 #set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings=os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 class Ping(Resource):
     def get(self):
